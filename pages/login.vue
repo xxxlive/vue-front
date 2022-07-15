60a51093508b17a83e0472604bb133ce829bc391
P0ng64 <56666973+P0ng64@users.noreply.github.com>
2022-07-15 23:35:17 +0800

    Beautify login and register page.

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main">
+  <div class="main" style="backdrop-filter: blur(10px)">
     <div class="title">
       <a class="active" href="/login">登录</a>
       <span>·</span>
@@ -8,7 +8,7 @@
     <div class="sign-up-container">
       <el-form ref="userForm" :model="user">
         <el-form-item class="input-prepend restyle" prop="mobile" :rules="[{ required: true, message: '请输入手机号码', trigger: 'blur' },{validator: checkPhone, trigger: 'blur'}]">
-          <div >
+          <div>
             <el-input type="text" placeholder="手机号" v-model="user.mobile"/>
             <i class="iconfont icon-phone" />
           </div>
@@ -34,64 +34,61 @@
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-  import '~/assets/css/sign.css'
-  import '~/assets/css/iconfont.css'
-  import cookie from 'js-cookie'
-  import loginApi from '@/api/login'
+import '~/assets/css/sign.css'
+import '~/assets/css/iconfont.css'
+import cookie from 'js-cookie'
+import loginApi from '@/api/login'
 
-  export default {
-    layout: 'sign',
-    data () {
-      return {
-        wxurl:"http://47.95.195.219:8160/educenter/api/ucenter/wx/login",
-        user:{
-          mobile:'',
-          password:''
-        },
-        loginInfo:{}
-      }
+export default {
+  layout: 'sign',
+  data () {
+    return {
+      wxurl:"http://47.95.195.219:8160/educenter/api/ucenter/wx/login",
+      user:{
+        mobile:'',
+        password:''
+      },
+      loginInfo:{}
+    }
+  },
+  methods: {
+    //登陆
+    submitLogin() {
+      //1.调用接口进行登陆,得到tocken字符串
+      loginApi.submitLogin(this.user)
+              .then(response => {
+                //2.将token字符串放到cookie里
+                           //key         value                    作用范围
+                 cookie.set('guli_token',response.data.data.token,{domain: '47.95.195.219'})
+                 //console.log('guli_token:'+cookie.get(guli_token))
+                //4.调用接口,根据token获取用户信息
+                loginApi.getLoginUserInfo()
+                        .then(response => {
+                           this.loginInfo = response.data.data.userInfo
+                           //获取返回用户信息，放到cookie里面
+                           cookie.set('guli_ucenter',this.loginInfo,{domain: '47.95.195.219'})
+                           //console.log('guli_ucenter:'+cookie.get(guli_ucenter))
+                           //跳转页面
+                           window.location.href="/"
+                        })
+              })
     },
-    methods: {
-      //登陆
-      submitLogin() {
-        //1.调用接口进行登陆,得到tocken字符串
-        loginApi.submitLogin(this.user)
-                .then(response => {
-                  //2.将token字符串放到cookie里
-                             //key         value                    作用范围
-                   cookie.set('guli_token',response.data.data.token,{domain: '47.95.195.219'})
-                   //console.log('guli_token:'+cookie.get(guli_token))
-                  //4.调用接口,根据token获取用户信息
-                  loginApi.getLoginUserInfo()
-                          .then(response => {
-                             this.loginInfo = response.data.data.userInfo
-                             //获取返回用户信息，放到cookie里面
-                             cookie.set('guli_ucenter',this.loginInfo,{domain: '47.95.195.219'})
-                             //console.log('guli_ucenter:'+cookie.get(guli_ucenter))
-                             //跳转页面
-                             window.location.href="/"
-                          })
-                })
-      },
-       //验证手机号码格式
-      checkPhone (rule, value, callback) {
-        if (!(/^1[34578]\d{9}$/.test(value))) {
-          return callback(new Error('手机号码格式不正确'))
-        }
-        return callback()
-      },
-      wxlogin(){
-        // loginApi.wxlogin()
+     //验证手机号码格式
+    checkPhone (rule, value, callback) {
+      if (!(/^1[34578]\d{9}$/.test(value))) {
+        return callback(new Error('手机号码格式不正确'))
       }
+      return callback()
+    },
+    wxlogin(){
+      // loginApi.wxlogin()
     }
   }
+}
 </script>
 <style>
-   .el-form-item__error{
-    z-index: 9999999;
-  }
+
 </style>
