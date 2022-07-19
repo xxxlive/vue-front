47a70b3b5873710e10a0bb971b9350c4eeca3b04
iiap <1471127927@qq.com>
2022-07-19 22:32:33 +0800

    修改课程列表样式

--- pages/course/index.vue
+++ pages/course/index.vue
@@ -76,29 +76,13 @@
           <article class="comm-course-list" v-if="data.total > 0">
             <ul class="of" id="bna">
               <li v-for="item in data.items" :key="item.id">
-                <div class="cc-l-wrap">
-                  <section class="course-img">
-                    <img :src="item.cover" class="img-responsive" :alt="item.title">
-                    <div class="cc-mask">
-                      <a :title="item.title" class="comm-btn c-btn-1" v-on:click="isLogin(item.id)">Start Learning</a>
-                    </div>
-                  </section>
-                  <h3 class="hLh30 txtOf mt10">
-                    <a :href="'/course/'+item.id" :title="item.title" class="course-title fsize18 c-333" v-on:click="isLogin(item.id)">{{item.title}}</a>
-                  </h3>
-                  <section class="mt10 hLh20 of">
-                    <span v-if="Number(item.price) === 0" class="fr jgTag bg-green">
-                      <i class="c-fff fsize12 f-fA">Free</i>
-                    </span>
-                    <span v-else class="fr jgTag bg-orange">
-                      <i class="c-fff fsize12 f-fA"> ¥{{item.price}}</i>
-                    </span>
-                    <span class="fl jgAttr c-ccc f-fA">
-                      <i class="c-999 f-fA">{{item.buyCount}} Brought</i>
-                      <i class="c-999 f-fA">{{item.viewCount}} Viewed</i>
-                    </span>
-                  </section>
-                </div>
+
+                <class_lable_veritical_white v-bind:cover="item.cover" v-bind:title="item.title" v-bind:price="item.price" v-bind:buycount="item.buyCount" v-bind:viewcount="item.viewCount"
+
+                                             v-on:islogin="isLogin(item.id)" >
+
+                </class_lable_veritical_white>
+
               </li>
             </ul>
             <div class="clear"></div>
@@ -149,7 +133,21 @@
 
 import courseApi from '@/api/course'
 import cookie from 'js-cookie'
+import class_label_vertical_white from "@/components/lable/class_label_vertical_white";
+import Class_lable_veritical_white from "@/components/lable/class_label_vertical_white";
+import class_lable_horizotal from "@/components/class_lable_horizotal";
+import class_lable_veritical from "@/components/lable/class_lable_veritical";
+
 export default {
+
+
+  components: {
+    Class_lable_veritical_white,
+    class_lable_horizotal,
+    class_lable_veritical
+
+  },
+
   data() {
     return {
         page:1,
