cc378d25e3d4a161eb733403ae3b4fa89ce745c0
iiap <1471127927@qq.com>
2022-07-20 17:03:08 +0800

    增加鸿濛图标

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,15 +6,15 @@ module.exports = {
       { src: '~/plugins/nuxt-swiper-plugin.js', ssr: false }
     ],
  head: {
-      title: '在线教育学院',
+      title: '鸿濛5G教育学院',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'keywords', name: 'keywords', content: '在线教育学院' },
-        { hid: 'description', name: 'description', content: '在线教育学院' }
+        { hid: 'keywords', name: 'keywords', content: '鸿濛5G教育学院' },
+        { hid: 'description', name: 'description', content: '鸿濛5G教育学院' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/hm.jpg' }
       ],
       script: [
         { src: '/js/aliplayercomponents-1.0.5.min.js'}
